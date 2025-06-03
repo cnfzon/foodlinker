5619c07b30d24c7fb8548c52185759e2f4cb702a
cnfzon <cnfzon.5@gmail.com>
2025-06-04 01:25:24 +0800

    Update page.tsx

--- app/weigh/page.tsx
+++ app/weigh/page.tsx
@@ -19,7 +19,7 @@ export default function WeighPage() {
           <h2 className={styles.historyTitle}>過去上傳記錄</h2>
           <ul className={styles.historyList}>
             <li>2024/06/01　300g</li>
-            <li>2024/05/28　800g</li>
+            <li>2024/05/28　900g</li>
           </ul>
         </div>
       </div>
